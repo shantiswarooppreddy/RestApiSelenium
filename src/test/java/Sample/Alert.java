eac29dea857bee96812b3e0c80c39f612ab0de17
shantiswarooppreddy <shanti.s.reddy@gmail.com>
2020-05-19 12:32:14 +0530

    Update Alert.java
    s

--- src/test/java/Sample/Alert.java
+++ src/test/java/Sample/Alert.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import ObjectRepository.Repository1;
 import TestBase.TestInitialization;
 
